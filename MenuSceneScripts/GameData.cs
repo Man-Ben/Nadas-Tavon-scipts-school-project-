5d82f4d35da5fa141d241b096b364934d44bcd5d
Man-Ben <143604908+Man-Ben@users.noreply.github.com>
2026-03-18 20:10:13 +0200

    Documented
    Added a comment explaining the purpose of the GameData class.

--- MenuSceneScripts/GameData.cs
+++ MenuSceneScripts/GameData.cs
@@ -1,6 +1,12 @@
+/*
+This class was only created because I had to transfer the data 
+between the main menu and the main scene.
+*/
+
+
 using UnityEngine;
 
 public static class GameData
-{
+{   
     public static float difficulty;
 }
